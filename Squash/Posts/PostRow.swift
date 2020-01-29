ce81c44af593cf02cff44d2457ac9b6801303f9c
corykacal <corykacal@utexas.edu>
2020-01-28 19:57:41 -0600

    started remkaing post

--- Squash/Posts/PostRow.swift
+++ Squash/Posts/PostRow.swift
@@ -38,139 +38,59 @@ struct PostRow: View {
     
     var body: some View {
         
-        HStack {
-            //Entire post stack
-            VStack(spacing: 10) {
-                //HStack to move content to the left using the trailing Spacer()
-                HStack {
-                    //VStack to stack the subject ontop of the contents
-                    VStack(alignment: .leading, spacing: 0) {
-                        ZStack(alignment: .leading) {
-
-                        if self.post.subject != nil {
-                            //ZStack to place the subject ontop of the rectangle
-                                Rectangle()
-                                    .size(width: 1000, height: 17)
-                                    .fill(Color.yellow)
-                                    .padding(.horizontal, -20)
-                                    .padding(.top, -5)
-                                //post subject
-                                Text(self.post.subject!.uppercased())
-                                    .bold()
-                                    .font(.system(size: 13))
-                                    .padding(.top, -5)
-                                    .foregroundColor(Color.white)
-                            }
-                        }
-
-                        
-                        
-                        //post contents
-                        Text(self.post.contents)
-                            .font(.system(size: 20))
-                            .foregroundColor(.primary)
-                            .padding(.top, 3.5)
-                    }
-                    .layoutPriority(100)
-
-                    Spacer()
-                    
-                }
-                .padding(.top, 5)
-                .padding(.horizontal, 6)
-                .padding(.leading, 3)
-                            
-                //Image attatched to the post
-                if self.post.imageuuid != nil {
-                    //display full image for single post or not
-                    if self.cropped {
-                        FirebaseImage(id: self.post.imageuuid!)
-                            .frame(minWidth: 0, idealWidth: 200, maxWidth: .infinity, minHeight: 130, idealHeight: 130, maxHeight:   200, alignment: .center)
-                            .clipShape(
-                            RoundedRectangle(cornerRadius: 10)
-                        )        .overlay(
-                            RoundedRectangle(cornerRadius: 10)
-                                .stroke(Color(.sRGB, red: 150/255, green: 150/255, blue: 150/255, opacity: 0.2), lineWidth: 1)
-                        )
-                        .padding([.horizontal], 20)
-                    } else {
-                        FirebaseImage(id: self.post.imageuuid!)
-                            
-                            .clipShape(
-                            RoundedRectangle(cornerRadius: 10)
-                        )
-                            .overlay(
-                                RoundedRectangle(cornerRadius: 10)
-                                    .stroke(Color(.sRGB, red: 150/255, green: 150/255, blue: 150/255, opacity: 0.2), lineWidth: 1)
-                            )
-                        .padding([.horizontal], 20)
-                    }
-                }
-
-                Spacer()
-                //HStack to move the timestamp and comment count to the left using the trailing Spacer()
-                HStack {
-                    Text(getTimeSince(date: self.post.timestamp))
-                        .font(.system(size: 13))
-                        .padding(.bottom, 5)
-                        .padding(.leading, 7)
-                        .opacity(0.3)
-                    
-                    Image(systemName: "text.bubble.fill")
-                        .font(.system(size: 12.0, weight: .bold))
-                        .foregroundColor(Color.black)
-                        .opacity(0.2)
-                        .padding(.bottom, 3)
-                        .padding(.trailing, -4)
-
-                    if self.post.commentCount != nil {
-                        Text(String(self.post.commentCount!))
-                            .font(.system(size: 13))
-                            .padding(.bottom, 5)
-                            .opacity(0.3)
-                    }
-                    
-                    Spacer()
+        VStack {
+               
+            if !(self.post.subject==nil) {
+                ZStack {
+                    Text(self.post.subject!)
+                    Rectangle().foregroundColor(Color.yellow)
                 }
             }
             
-            
-            VStack {
-                Image(systemName: "chevron.up")
-                    .font(.system(size: 29, weight: .medium))
-                    .foregroundColor(self.decision==true ? Color.green : Color.black)
-                    .opacity(0.4)
-                    .padding(.top, 15)
-                    .onTapGesture {
-                        if(self.decision==true) {
-                            self.decision = nil
-                            self.mainViewModel.makeDecision(decision: nil, post_number: self.post.id)
+            HStack {
+                
+                VStack {
+                    Text(post.contents)
+                    //Image attatched to the post
+                    if self.post.imageuuid != nil {
+                        //display full image for single post or not
+                        if self.cropped {
+                            FirebaseImage(id: self.post.imageuuid!)
+                                .frame(minWidth: 0, idealWidth: 200, maxWidth: .infinity, minHeight: 130, idealHeight: 130, maxHeight:   200, alignment: .center)
+                                .clipShape(
+                                RoundedRectangle(cornerRadius: 10)
+                            )        .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(Color(.sRGB, red: 150/255, green: 150/255, blue: 150/255, opacity: 0.2), lineWidth: 1)
+                            )
+                            .padding([.horizontal], 20)
                         } else {
-                            self.decision = true
-                            self.mainViewModel.makeDecision(decision: true, post_number: self.post.id)
+                            FirebaseImage(id: self.post.imageuuid!)
+                                
+                                .clipShape(
+                                RoundedRectangle(cornerRadius: 10)
+                            )
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 10)
+                                        .stroke(Color(.sRGB, red: 150/255, green: 150/255, blue: 150/255, opacity: 0.2), lineWidth: 1)
+                                )
+                            .padding([.horizontal], 20)
                         }
+                        
+                        Text(getTimeSince(date: self.post.timestamp))
                     }
+                }
+                
                 Spacer()
-                Text(String(self.post.points))
-                    .foregroundColor((self.post.points<0) ? Color.red : Color.green)
-                    .padding(.trailing, 1)
-                Spacer()
-                Image(systemName: "chevron.down")
-                    .font(.system(size: 29, weight: .medium))
-                    .foregroundColor(self.decision==false ? Color.red : Color.black)
-                    .opacity(0.4)
-                    .padding(.bottom, 6)
-                    .onTapGesture {
-                        if(self.decision==false) {
-                            self.decision = nil
-                            self.mainViewModel.makeDecision(decision: nil, post_number: self.post.id)
-                        } else {
-                            self.decision = false
-                            self.mainViewModel.makeDecision(decision: false, post_number: self.post.id)
-                        }
-                    }
-            }.padding(.trailing, 8)
-            .onAppear(perform: setStates)
+                VStack {
+                    Image(systemName: "chevron.up")
+                    Text(String(self.post.points))
+                    Image(systemName: "chevron.down")
+                }
+                
+            }
+
+ 
 
         }
         .cornerRadius(10)
